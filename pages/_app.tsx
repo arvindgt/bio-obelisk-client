5dbf446871f836a3bda25ce2dfe3549d45efce5b
Arvind Kushwaha <arvindkshwh939@gmail.com>
2022-05-01 18:19:19 +0530

    integration in react - added bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
+import '../styles/Home.module.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
